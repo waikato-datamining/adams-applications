a8bb7a976ed04a580630800872609a42604cc331
fracpete <fracpete@gmail.com>
2016-09-10 22:25:47 +0000

    added "pure" ml application

---
+++ adams-ml-app/src/main/scripts/run.bat
@@ -0,0 +1,3 @@
+@echo off
+
+%~dp0\launcher.bat -main adams.gui.Main -memory 2g -title "ADAMS Spectral"
---
+++ adams-ml-app/src/main/scripts/terminal.bat
@@ -0,0 +1,3 @@
+@echo off
+
+java -cp "%~dp0\..\lib\*" -Xmx2g adams.terminal.Main -title "ADAMS Spectral"
