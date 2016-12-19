0296e460f34bcaac8ed5b3b33e92eadb138078a5
fracpete <fracpete@gmail.com>
2016-12-19 21:15:24 +0000

    added javadoc, tooltip methods

--- adams-annotator/src/main/java/adams/data/boofcv/transformer/BinaryMask.java
+++ adams-annotator/src/main/java/adams/data/boofcv/transformer/BinaryMask.java
@@ -23,60 +23,125 @@ package adams.data.boofcv.transformer;
 import adams.data.boofcv.BoofCVHelper;
 import adams.data.boofcv.BoofCVImageContainer;
 import adams.data.boofcv.BoofCVImageType;
-import adams.data.boofcv.transformer.AbstractBoofCVTransformer;
 import adams.data.image.AbstractImageContainer;
 import adams.flow.control.StorageName;
-import boofcv.struct.image.ImageBase;
 import boofcv.struct.image.ImageUInt8;
-import boofcv.struct.image.MultiSpectral;
 
 import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
 
 /**
- * TODO: what class does.
+ <!-- globalinfo-start -->
+ * Masks an image using a provided binary mask from storage.
+ * <br><br>
+ <!-- globalinfo-end -->
+ *
+ <!-- options-start -->
+ * <pre>-logging-level &lt;OFF|SEVERE|WARNING|INFO|CONFIG|FINE|FINER|FINEST&gt; (property: loggingLevel)
+ * &nbsp;&nbsp;&nbsp;The logging level for outputting errors and debugging output.
+ * &nbsp;&nbsp;&nbsp;default: WARNING
+ * </pre>
+ * 
+ * <pre>-mask &lt;adams.flow.control.StorageName&gt; (property: mask)
+ * &nbsp;&nbsp;&nbsp;The name of the mask in internal storage.
+ * &nbsp;&nbsp;&nbsp;default: storage
+ * </pre>
+ * 
+ * <pre>-high &lt;boolean&gt; (property: high)
+ * &nbsp;&nbsp;&nbsp;If enabled, uses 255 as mask color instead of 0.
+ * &nbsp;&nbsp;&nbsp;default: false
+ * </pre>
+ * 
+ <!-- options-end -->
  *
  * @author sjb90
  * @version $Revision$
  */
-public class BinaryMask extends AbstractBoofCVTransformer {
+public class BinaryMask
+  extends AbstractBoofCVTransformer {
+
+  private static final long serialVersionUID = -4518424819623795118L;
+
+  /** use high or low value for mask **/
+  protected boolean m_High;
 
   /** the image in internal storage. */
   protected StorageName m_Mask;
 
+  /**
+   * Returns a string describing the object.
+   *
+   * @return a description suitable for displaying in the gui
+   */
+  @Override
+  public String globalInfo() {
+    return "Masks an image using a provided binary mask from storage.";
+  }
+
+  /**
+   * Adds options to the internal list of options.
+   */
+  public void defineOptions() {
+    super.defineOptions();
+    m_OptionManager.add("mask", "mask", new StorageName());
+    m_OptionManager.add("high", "high", false);
+  }
+
+  /**
+   * Sets whether to use 0 (low) or 255 (high) as mask color.
+   *
+   * @param value	true if to use high mask color
+   */
+  public void setHigh(boolean value) {
+    m_High = value;
+    reset();
+  }
+
+  /**
+   * Returns whether to use low/high mask color.
+   *
+   * @return		true if to use high mask color
+   */
   public boolean isHigh() {
     return m_High;
   }
 
-  public void setHigh(boolean m_High) {
-    this.m_High = m_High;
+  /**
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
+   */
+  public String highTipText() {
+    return "If enabled, uses 255 as mask color instead of 0.";
   }
 
-  /** use high or low value for mask **/
-  protected boolean m_High;
   /**
-   * Getter method for the mask
-   * @return the mask name
+   * Set the mask name in storage.
+   *
+   * @param value	the name
+   */
+  public void setMask(StorageName value) {
+    m_Mask = value;
+    reset();
+  }
+
+  /**
+   * Getter method for the mask storage name.
+   *
+   * @return 		the mask name
    */
   public StorageName getMask() {
     return m_Mask;
   }
 
   /**
-   * Set the mask name
-   * @param m_Mask
+   * Returns the tip text for this property.
+   *
+   * @return 		tip text for this property suitable for
+   * 			displaying in the GUI or for listing the options.
    */
-  public void setMask(StorageName m_Mask) {
-    this.m_Mask = m_Mask;
-  }
-
-  public void defineOptions() {
-    super.defineOptions();
-    m_OptionManager.add("mask", "mask", new StorageName());
-    m_OptionManager.add("high", "high", false);
+  public String maskTipText() {
+    return "The name of the mask in internal storage.";
   }
 
   /**
@@ -89,8 +154,8 @@ public class BinaryMask extends AbstractBoofCVTransformer {
   protected BoofCVImageContainer[] doTransform(BoofCVImageContainer img) {
     ImageUInt8			maskImg;
     Object			obj;
-    BoofCVImageContainer 	container = new BoofCVImageContainer();
-    BufferedImage image;
+    BoofCVImageContainer 	container;
+    BufferedImage 		image;
 
     if (getFlowContext() == null)
       throw new IllegalStateException("No flow context set!");
@@ -108,6 +173,8 @@ public class BinaryMask extends AbstractBoofCVTransformer {
 	"Mask must be either a " + BufferedImage.class.getName()
 	  + " or derived from " + AbstractImageContainer.class.getName()
 	  + ", found: " + obj.getClass().getName());
+
+    container = new BoofCVImageContainer();
     container.setImage(maskImg);
 
     // check that images are the same size
@@ -116,8 +183,6 @@ public class BinaryMask extends AbstractBoofCVTransformer {
 
     if(!BoofCVHelper.isBinary(container))
       throw new IllegalStateException("Mask must be binary");
-    byte[] data = maskImg.getData();
-
 
     image = img.toBufferedImage();
     int maskColour = 0;
@@ -136,15 +201,5 @@ public class BinaryMask extends AbstractBoofCVTransformer {
     result[0] = new BoofCVImageContainer();
     result[0].setImage(BoofCVHelper.toBoofCVImage(image));
     return result;
-  }
-
-  /**
-   * Returns a string describing the object.
-   *
-   * @return a description suitable for displaying in the gui
-   */
-  @Override
-  public String globalInfo() {
-    return "Masks an image using a provided binary mask";
   }
 }
