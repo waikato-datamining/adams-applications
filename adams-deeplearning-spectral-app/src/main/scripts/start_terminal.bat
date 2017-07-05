22f573685dcd73ce0d4b135f39159b5efb1550c0
fracpete <fracpete@gmail.com>
2017-07-05 21:29:39 +0000

    added spectral-app with deeplearning support

---
+++ adams-deeplearning-spectral-app/src/main/scripts/start_terminal.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"%~dp0\terminal.bat" -memory 2g -main adams.terminal.Main -title ADAMS-Deeplearning-Spectral
---
+++ adams-deeplearning-spectral-app/src/main/scripts/start_terminal.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+`dirname $0`/terminal.sh -memory 2g -main adams.terminal.Main -title ADAMS-Deeplearning-Spectral
