6ee3b245ebaf7f20aaf6a5dc9fed1bf41895a22a
Peter Reutemann <fracpete@gmail.com>
2022-06-20 13:33:11 +1200

    increased heap to 2GB

--- adams-annotator/src/main/scripts/start_gui.bat
+++ adams-annotator/src/main/scripts/start_gui.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"%~dp0\launcher.bat" -main adams.gui.Main -memory 1g -title ADAMS-Annotator -env-modifier adams.core.management.WekaHomeEnvironmentModifier
+"%~dp0\launcher.bat" -main adams.gui.Main -memory 2g -title ADAMS-Annotator -env-modifier adams.core.management.WekaHomeEnvironmentModifier
