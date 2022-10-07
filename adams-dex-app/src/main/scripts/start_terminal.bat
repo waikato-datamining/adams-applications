8a778da1dc057e2ce4b064465bd8fc560b6682a3
Peter Reutemann <fracpete@gmail.com>
2022-10-07 14:43:16 +1300

    retired RabbitMQ modules and DEX app

--- adams-dex-app/src/main/scripts/start_terminal.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"%~dp0\terminal.bat" -memory 512m -main adams.terminal.Main -title ADAMS-Basic
--- adams-dex-app/src/main/scripts/start_terminal.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-`dirname $0`/terminal.sh -memory 512m -main adams.terminal.Main -memory 512m -title ADAMS-Basic
